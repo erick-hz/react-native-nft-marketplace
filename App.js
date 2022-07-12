1267442392ab15f53ef4a3ea9fb1ecd838245353
Erick Hernandez <yerickk8@gmail.com>
2022-07-12 09:14:48 -0500

    Set up cpmponents and screens and added assest folder

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-// import { useFonts } from '/screen';
+import { useFonts } from 'expo-font';
 import Home from './screens/Home';
 import Details from './screens/Details';
 
@@ -16,6 +16,16 @@ const theme = {
 }
 
 const App = () => {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+    if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home" >
---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { useIsFocused } from "@react-navigation/core";
+
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused();
+
+  return isFocused ? 
+    <StatusBar barStyle={"light-content"} animated={true} {...props} /> : null;
+};
+
+export default FocusedStatusBar;
+
