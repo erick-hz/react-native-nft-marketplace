97e8b10876914d04bdb50653f38b2a26c10136aa
Erick Hernandez <yerickk8@gmail.com>
2022-11-17 10:46:48 -0600

    commit

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import Home from './screens/Home';
-import Details from './screens/Details';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
-const Stack = createStackNavigator();
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
 const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
     background: "transparent",
+  },
+};
 
-  }
-}
+const Stack = createStackNavigator();
 
 const App = () => {
   const [loaded] = useFonts({
@@ -24,21 +25,21 @@ const App = () => {
     InterLight: require("./assets/fonts/Inter-Light.ttf"),
   });
 
-    if (!loaded) return null;
+  if (!loaded) return null;
 
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home" >
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Details" component={Details} />
-
       </Stack.Navigator>
     </NavigationContainer>
-
-
   );
-}
+};
 
 export default App;
-
-
